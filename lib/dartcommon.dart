81cd37eed71aa5fa6b05450da883a3086e88fa5e
zhangshengyu <zhang.shengyu@tlkg.com>
2020-05-28 16:32:17 +0800

    测试

---
+++ lib/dartcommon.dart
@@ -0,0 +1,9 @@
+part of dartcommon;
+
+class TestAdd {
+  /// Returns [value] plus 1.
+  Future<int> a_plus_b(int a,int b) async{
+    print('a+b:${a+b}');
+  return a+b;
+}
+}
---
+++ lib/testcommon.dart
@@ -0,0 +1,9 @@
+library dartcommon;
+part 'dartcommon.dart';
+
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
